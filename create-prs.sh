cbf82b0b54c11ff02e2221b9642e64f77cc2bdcf
Jonathan Tierno <jtierno@medallia.com>
2018-06-13 13:15:43 -0300

    create-prs.sh

--- create-prs.sh
+++ create-prs.sh
@@ -1,3 +1,9 @@
 CURRENT_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-BRANCH="$PRERFIX-1"
-curl -s -X POST --connect-timeout 60 --max-time 60  -H "Authorization: token $TOKEN" -d "{'tittle':'PR FOR $BRANCH','head':'$BRANCH','base':'$CURRENT_BRANCH','body':'', 'mantainers_can_modify': true}" https://api.github.com/repos/jontierno/bulldozer-poc/pulls
+BRANCH="$PREFIX-1"
+curl -s -X POST --connect-timeout 60 --max-time 60  -H "Authorization: token $TOKEN" -d "{\"title\":\"PR FOR $BRANCH\",\"head\":\"$BRANCH\",\"base\":\"$CURRENT_BRANCH\",\"body\":\"\", \"mantainers_can_modify\": \"true\"}" https://api.github.com/repos/jontierno/bulldozer-poc/pulls
+
+BRANCH="$PREFIX-2"
+curl -s -X POST --connect-timeout 60 --max-time 60  -H "Authorization: token $TOKEN" -d "{\"title\":\"PR FOR $BRANCH\",\"head\":\"$BRANCH\",\"base\":\"$CURRENT_BRANCH\",\"body\":\"\", \"mantainers_can_modify\": \"true\"}" https://api.github.com/repos/jontierno/bulldozer-poc/pulls
+
+BRANCH="$PREFIX-3"
+curl -s -X POST --connect-timeout 60 --max-time 60  -H "Authorization: token $TOKEN" -d "{\"title\":\"PR FOR $BRANCH\",\"head\":\"$BRANCH\",\"base\":\"$CURRENT_BRANCH\",\"body\":\"\", \"mantainers_can_modify\": \"true\"}" https://api.github.com/repos/jontierno/bulldozer-poc/pulls
