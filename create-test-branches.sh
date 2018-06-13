f6c912e754aa2fa33ebb377b5717fd0727cc857c
Jonathan Tierno <jtierno@medallia.com>
2018-06-13 15:35:23 -0300

    fix pr creation:

--- create-test-branches.sh
+++ create-test-branches.sh
@@ -2,21 +2,21 @@ INITIAL_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 export BASE_BRANCH=$INITIAL_BRANCH
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
-BRANCH="$PREFIX-1"
+export BRANCH="$PREFIX-1"
 git checkout -b "$BRANCH"
 git push -u origin $BRANCH
 $DIR/commit-file.sh
 $DIR/create-pr.sh
 
 git checkout $INITIAL_BRANCH
-BRANCH="$PREFIX-2"
+export BRANCH="$PREFIX-2"
 git checkout -b "$BRANCH"
 git push -u origin $BRANCH
 $DIR/commit-file.sh
 $DIR/create-pr.sh
 
 git checkout $INITIAL_BRANCH
-BRANCH="$PREFIX-3"
+export BRANCH="$PREFIX-3"
 git checkout -b "$BRANCH"
 git push -u origin $BRANCH
 $DIR/commit-file.sh
